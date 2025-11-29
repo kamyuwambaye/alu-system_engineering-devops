c065675d768a26d61f68bfad8c134dc2b32d7c3f
kamyuwambaye <k.uwambaye@alustudent.com>
2025-11-29 19:02:40 +0200

    Fix web_stack_debugging_4 Puppet manifests to properly handle 1000 requests with 100 concurrent connections

--- web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -2,5 +2,5 @@
 
 exec { 'replace_limit':
   path    => '/usr/bin:/usr/sbin:/bin',
-  command => 'sed -i "/ULIMIT=/c\ULIMIT=\'-n 4096\'" /etc/default/nginx; service nginx restart',
+  command => 'sed -i "s/^ULIMIT.*/ULIMIT=\"-n 1024\"/" /etc/default/nginx; service nginx restart',
 }
--- web_stack_debugging_4/1-user_limit.pp
+++ web_stack_debugging_4/1-user_limit.pp
@@ -2,6 +2,5 @@
 
 exec { 'replace_hard_soft_limit_for_holberton_user':
   path    => '/usr/bin:/usr/sbin:/bin',
-  command => 'sed -i "/holberton hard nofile/c\holberton hard nofile 4096" /etc/security/limits.conf;\
-sed -i "/holberton soft nofile/c\holberton soft nofile 1024" /etc/security/limits.conf',
+  command => 'echo "holberton hard nofile 4096" >> /etc/security/limits.conf; echo "holberton soft nofile 1024" >> /etc/security/limits.conf',
 }
